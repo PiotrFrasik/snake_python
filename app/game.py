24f621943c89bf1064f3c85cd096d03dd2cbae72
PiotrFrasik <frasik.piotrek@gmail.com>
2025-11-24 22:17:43 +0100

    Add terminal size validation

--- app/game.py
+++ app/game.py
@@ -6,12 +6,17 @@ from core.start_window import WelcomeWindow
 class StartGame:
     def __init__(self):
         self.screen = curses.initscr()
-
         self.board = BoardGame(self.screen)
 
     def main(self, stdscr):
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
         neon_green = curses.color_pair(1)
+
+        rows,cols = self.screen.getmaxyx()
+        #terminal size validation 
+        if not(rows == 20 and cols == 80):
+            raise Exception("Terminal size must be exactly 20 and 80 (rows x cols).\n"
+                            f"Current size: {rows} x {cols}.")
 
         start_window = WelcomeWindow(self.screen, neon_green)
 
@@ -22,6 +27,7 @@ class StartGame:
         self.board.draw(neon_green)
 
         time.sleep(5)
+
 
 if __name__ == "__main__":
     curses.wrapper(StartGame().main)
